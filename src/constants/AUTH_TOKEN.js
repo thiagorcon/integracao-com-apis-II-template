50a62b8c21ee757136fa64113fb6cf4ef4de7a66
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2023-04-08 22:16:57 -0300

    Revert "Branch atualizacao template"

--- src/constants/AUTH_TOKEN.js
+++
@@ -1,4 +0,0 @@
-// Arquivo para guardar o token do usuário .
-// Colocar o seu token  
-
-export const AUTH_TOKEN = " "
--- src/constants/BASE_URL.js
+++
@@ -1,3 +0,0 @@
-// Arquivo para guardar o a constantes de base da url .
-
-export const BASE_URL = 'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users';
